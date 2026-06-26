bee931fa7834d9783e4a331f48162691e63acf63
Ansh <altatrescue@gmail.com>
2026-06-27 01:42:35 +0530

    Initial Flask setup with User and Group models

---
+++ models/group.py
@@ -0,0 +1,10 @@
+from .db import db
+
+class Group(db.Model):
+    __tablename__ = "groups"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"<Group {self.name}>"
